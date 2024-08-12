5a026a4e17e617ba5a22d4b6e5f1acf6348b010a
Zhong Jiecheng <21302010025@m.fudan.edu.cn>
2024-08-12 16:04:06 +0800

    catchup

--- SimpleAnimateExample/main.cpp
+++ SimpleAnimateExample/main.cpp
@@ -4,49 +4,53 @@
 #include <control/FPScontrol.h>
 #include <geometry/BoxGeometry.h>
 #include <material/CommonMaterial.h>
-#include <Renderer.h>
-#include <Scene.h>
+#include <core/Renderer.h>
+#include <core/Scene.h>
+#include <core/Window.h>
+#include <core/Context.h>
 #include <texture/ImageTexture.h>
 
-#include <Context.h>
+
+#include <util/Logger.h>
 using namespace SA;
 
 int main()
 {
-
-	Context ctx;
-	Renderer renderer;
-	FPSControl control;
-	PerspecCamera camera(45.0, 1920.0 / 1080.0, 0.01, 100.0);
-	camera.setPosition(glm::vec3(0, 0, -1));
-
-
-	//control.bindObject(&camera);
-	//control.bindContext(&ctx);
+  Window window(1920, 1080, 400, 200);
+  Context& ctx = window.context;
+  Renderer renderer;
+  FPSControl control;
+  PerspecCamera camera(45.0, 1920.0 / 1080.0, 0.01, 100.0);
 
 
-	Scene scene;
-
-	auto* box = new Object3D(
-		new BoxGeometry(),
-		new CommonMaterial(
-			{
-				{"diffuse", new ImageTexture("static/image/awesomeface.png")}
-			}
-		)
-	);
-
-	scene.add(box);
-
-	ctx.loop([&]() {
-
-		//control.inputLoop(&ctx);
-		camera.setPosition(glm::vec3(0, 0, ctx.getTime() < 10 ? ctx.getTime():10));
-		renderer.render(scene, camera);
-	});
-
-	//ctx.loop([]() {});
+  camera.setPosition(glm::vec3(0, 0, 5));
+  control.bindObject(&camera);
+  control.bindWindow(&window);
+  control.mouse_sensitivity = 0.002;
 
 
-	return 0;
+  Scene scene;
+
+  auto* box = new Object3D(new BoxGeometry(),
+    new CommonMaterial(
+      {
+        {"diffuse", new ImageTexture("static/image/awesomeface.png")}
+      }
+    )
+  );
+
+  scene.add(box);
+
+  ctx.loop = [&](double delta) {
+
+
+    logger.log(camera.getDirection(), "main()::cam dir");
+    renderer.render(scene, camera);
+  };
+
+  ctx.loop.get()(0.0);
+
+
+
+  return 0;
 }
