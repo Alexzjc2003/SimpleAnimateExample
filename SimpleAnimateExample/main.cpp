5933c9c2c60bf50e56828e00094af18933255ce5
Zhong Jiecheng <21302010025@m.fudan.edu.cn>
2024-08-20 17:12:07 +0800

    1

--- SimpleAnimateExample/main.cpp
+++ SimpleAnimateExample/main.cpp
@@ -1,56 +1,32 @@
 #include <iostream>
 
-#include <camera/PerspecCamera.h>
-#include <control/FPScontrol.h>
-#include <geometry/BoxGeometry.h>
-#include <material/CommonMaterial.h>
-#include <core/Renderer.h>
-#include <core/Scene.h>
-#include <core/Window.h>
-#include <core/Context.h>
-#include <texture/ImageTexture.h>
-
-
 #include <util/Logger.h>
+
+#include <control/FPSControl.h>
+#include <core/Window.h>
+#include <geometry/BoxGeometry.h>
+#include <material/BlinnPhongMaterial>
+
 using namespace SA;
 
 int main()
 {
-  Window window(1920, 1080, 400, 200);
+  // environment setting
+  Window window;
   Context& ctx = window.context;
-  Renderer renderer;
   FPSControl control;
-  PerspecCamera camera(45.0, 1920.0 / 1080.0, 0.01, 100.0);
+  window.bindControl(&control);
+
+  // A box, with blinn-phong material
+  Object3D box(
+    BoxGeometry(),
+  )
+  
 
 
-  camera.setPosition(glm::vec3(0, 0, 5));
-  control.bindObject(&camera);
-  control.bindWindow(&window);
-  control.mouse_sensitivity = 0.002;
 
 
-  Scene scene;
-
-  auto* box = new Object3D(new BoxGeometry(),
-    new CommonMaterial(
-      {
-        {"diffuse", new ImageTexture("static/image/awesomeface.png")}
-      }
-    )
-  );
-
-  scene.add(box);
-
-  ctx.loop = [&](double delta) {
-
-
-    logger.log(camera.getDirection(), "main()::cam dir");
-    renderer.render(scene, camera);
-  };
-
-  ctx.loop.get()(0.0);
-
-
+  //system("pause");
 
   return 0;
 }
